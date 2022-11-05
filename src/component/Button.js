122a066d104a88ae7bcae9d85d5e6f564d238257
Muhdsodiq Bolarinwa <105107754+Amity808@users.noreply.github.com>
2022-11-04 22:47:51 -0700

    Update Button.js
    Another update on the Button

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './Button.css';
+import Footer from './footer';
 
 function Button(props) {
   return (
@@ -15,11 +17,15 @@ function Button(props) {
         <a href='https://books.zuri.team/python-for-beginners?ref_id=Amity' id='book__python'>{props.python}</a>
         <a href='https://background.zuri.team' id='pitch'>{props.content}</a>
         <a href='https://books.zuri.team/design-rules' id='book__design'>{props.design}</a>
+        <a href="/contact">{props.contact}</a>
+        <Link to="/contact">{props.contact}</Link>
         </div>
+          
         <div>
           <img src="./images/slack.png" alt="zuri" id='social-media'/>
           <img src="./images/github.png" alt="git" id='social-media'/>
         </div>
+        <Footer />
     </div>
   )
 }
